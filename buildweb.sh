2a30b3a78b1e77abf84e51a933d70972532ca61e
rocky08808 <96861481+rocky08808@users.noreply.github.com>
2022-04-21 21:13:16 +0800

    1

--- buildweb.sh
+++ buildweb.sh
@@ -1,8 +1,5 @@
+hexo clean
+echo '=== clean === done'
+
 hexo g
 echo '=== build === done'
-rm -rf docs
-echo '=== remove docs === done'
-mkdir docs
-echo '=== create docs === done'
-cp -r public/* docs/
-echo '=== public copy to docs === done'
